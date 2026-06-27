9bd309516f73c41c7026ab2003219fee3786c2ba
y4gg <contact@y4.gg>
2026-06-27 13:26:53 +0200

    setup drizzle & betterauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
-import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
-export default function Page() {
+export default function HomePage() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">
+        Hi, this landing page is still being built
+      </h1>
+      <Link href={"/app"}>Click me to visit the app</Link>
     </div>
   )
 }
---
+++ server/db/index.ts
@@ -0,0 +1,4 @@
+import "dotenv/config"
+import { drizzle } from "drizzle-orm/bun-sql"
+
+export const db = drizzle(process.env.DATABASE_URL!)
