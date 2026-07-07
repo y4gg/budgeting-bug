47fdead99a7629dbd1ced0cafed3cfdea407330e
y4gg <contact@y4.gg>
2026-07-07 23:13:04 +0200

    Create transactions page & add a link to overview

--- app/app/transactions/page.tsx
+++ app/app/transactions/page.tsx
@@ -1,14 +1,18 @@
 import { auth } from "@/lib/auth"
 import { headers } from "next/headers"
-import Link from "next/link"
 import { redirect } from "next/navigation"
 
 export default async function Overview() {
   const session = await auth.api.getSession({
-    headers: await headers(), // you need to pass the headers object.
+    headers: await headers(),
   })
 
   if (!session) redirect("/login")
 
-  return <Link href="/transactions">Only transactions are functional rn</Link>
+  return (
+    <div>
+      <h1>Transactions</h1>
+      <p>Welcome, {session.user.email}</p>
+    </div>
+  )
 }
